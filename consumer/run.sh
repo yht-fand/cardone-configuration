41dd4fdd2171bd7009171fd010f9db81fe1f5ad0
yao hai tao <addday@gmail.com>
2016-01-20 15:23:34 +0800

    init

--- consumer/run.sh
+++ consumer/run.sh
@@ -2,5 +2,5 @@
 echo run
 myself="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd jdk && set JAVA_HOME=$myself\jdk && PATH=$JAVA_HOME\bin:$PATH && cd $myself
-call java -Dfile.encoding=UTF-8 -cp $myself\*;$myself\target\* org.springframework.boot.loader.JarLauncher --app.root=$myself
-call run.sh
+java -Dfile.encoding=UTF-8 -cp $myself\*;$myself\target\* org.springframework.boot.loader.JarLauncher --app.root=$myself
+./run.sh
