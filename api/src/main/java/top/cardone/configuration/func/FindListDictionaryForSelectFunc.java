ff0b6a4b81bbcbd61234b606f19aa265eb6b4427
addday@gmail.com <addday@gmail.com>
2016-09-08 14:59:44 +0800

    79 系统主界面接口开发及联调：涉及到控制权限的菜单数据查询，还需先初始化菜单数据、角色数据、操作员与角色、角色与菜单等数据

---
+++ api/src/main/java/top/cardone/configuration/func/FindListDictionaryForSelectFunc.java
@@ -0,0 +1,43 @@
+package top.cardone.configuration.func;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import lombok.Setter;
+import org.apache.commons.collections.MapUtils;
+import top.cardone.configuration.service.DictionaryService;
+import top.cardone.context.ApplicationContextHolder;
+import top.cardone.core.util.func.Func1;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by cardone-home-001 on 2016/4/20.
+ */
+public class FindListDictionaryForSelectFunc implements Func1<List<Map<String, Object>>, Map<String, Object>> {
+	@Setter
+	private String dictionaryTypeCodeKeyName = "typeCode";
+
+	@Override
+	public List<Map<String, Object>> func(Map<String, Object> params) {
+		Map<String, Object> findListMap = Maps.newHashMap();
+
+		findListMap.put("dictionaryTypeCode", MapUtils.getString(params, dictionaryTypeCodeKeyName));
+		findListMap.put("order_by_order", "1");
+
+		List<Map<String, Object>> dictionaryList = ApplicationContextHolder.getBean(DictionaryService.class).findListCache(findListMap);
+
+		List<Map<String, Object>> newDictionaryList = Lists.newArrayList();
+
+		for (Map<String, Object> dictionary : dictionaryList) {
+			Map<String, Object> newDictionary = Maps.newHashMap();
+
+			newDictionary.put("text", MapUtils.getString(dictionary, "NAME"));
+			newDictionary.put("value", MapUtils.getString(dictionary, "DICTIONARY_CODE"));
+
+			newDictionaryList.add(newDictionary);
+		}
+
+		return newDictionaryList;
+	}
+}
