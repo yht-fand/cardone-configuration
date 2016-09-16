244650f1e6df53d1ff817b6a9a49310039e08a28
addday@gmail.com <addday@gmail.com>
2016-09-16 17:23:06 +0800

    添加读取数据字典类型下拉数据

---
+++ api/src/main/java/top/cardone/configuration/func/FindListDictionaryTypeForSelectFunc.java
@@ -0,0 +1,44 @@
+package top.cardone.configuration.func;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import lombok.Setter;
+import org.apache.commons.collections.MapUtils;
+import top.cardone.configuration.service.DictionaryService;
+import top.cardone.configuration.service.DictionaryTypeService;
+import top.cardone.context.ApplicationContextHolder;
+import top.cardone.core.util.func.Func1;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by cardone-home-001 on 2016/4/20.
+ */
+public class FindListDictionaryTypeForSelectFunc implements Func1<List<Map<String, Object>>, Map<String, Object>> {
+	@Setter
+	private String parentCodeKeyName = "parentCode";
+
+	@Override
+	public List<Map<String, Object>> func(Map<String, Object> params) {
+		Map<String, Object> findListMap = Maps.newHashMap();
+
+		findListMap.put("parentCode", MapUtils.getString(params, parentCodeKeyName));
+		findListMap.put("order_by_order", "1");
+
+		List<Map<String, Object>> dictionaryTypeList = ApplicationContextHolder.getBean(DictionaryTypeService.class).findListCache(findListMap);
+
+		List<Map<String, Object>> newDictionaryTypeList = Lists.newArrayList();
+
+		for (Map<String, Object> dictionaryType : dictionaryTypeList) {
+			Map<String, Object> newDictionaryType = Maps.newHashMap();
+
+			newDictionaryType.put("text", MapUtils.getString(dictionaryType, "NAME"));
+			newDictionaryType.put("value", MapUtils.getString(dictionaryType, "DICTIONARY_TYPE_CODE"));
+
+			newDictionaryTypeList.add(newDictionaryType);
+		}
+
+		return newDictionaryTypeList;
+	}
+}
---
+++ api/src/main/java/top/cardone/configuration/func/ReadOneDictionaryTypeFunc.java
@@ -0,0 +1,37 @@
+package top.cardone.configuration.func;
+
+import com.google.common.collect.Maps;
+import lombok.Setter;
+import org.apache.commons.collections.MapUtils;
+import top.cardone.configuration.service.DictionaryService;
+import top.cardone.configuration.service.DictionaryTypeService;
+import top.cardone.context.ApplicationContextHolder;
+import top.cardone.core.util.func.Func1;
+
+import java.util.Map;
+
+/**
+ * Created by cardone-home-001 on 2016/4/20.
+ */
+public class ReadOneDictionaryTypeFunc implements Func1<String, Map<String, Object>> {
+	@Setter
+	private String dictionaryTypeCodeKeyName = "code";
+	@Setter
+	private String objectIdKeyName = "objectId";
+
+	@Override
+	public String func(Map<String, Object> params) {
+		Map<String, Object> readOneMap = Maps.newHashMap();
+
+		readOneMap.put("dictionaryTypeCode", MapUtils.getString(params, dictionaryTypeCodeKeyName));
+		readOneMap.put("object_id", MapUtils.getString(params, objectIdKeyName));
+
+		boolean cache = MapUtils.getBooleanValue(params, "cache", true);
+
+		if (cache) {
+			return ApplicationContextHolder.getBean(DictionaryTypeService.class).readOneCache(String.class, readOneMap);
+		}
+
+		return ApplicationContextHolder.getBean(DictionaryTypeService.class).readOne(String.class, readOneMap);
+	}
+}
