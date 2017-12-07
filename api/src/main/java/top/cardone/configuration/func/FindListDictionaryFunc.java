c1ad90340b34fe2a1e23d6edd54265c27c153f58
addday <addday@gmail.com>
2017-12-07 15:23:00 +0800

    日志记录改进

---
+++ api/src/main/java/top/cardone/configuration/func/FindListDictionaryFunc.java
@@ -0,0 +1,15 @@
+package top.cardone.configuration.func;
+
+import top.cardone.configuration.service.DictionaryService;
+import top.cardone.context.ApplicationContextHolder;
+import top.cardone.core.util.func.Func1;
+
+import java.util.List;
+import java.util.Map;
+
+public class FindListDictionaryFunc implements Func1<List<Map<String, Object>>, Map<String, Object>> {
+    @Override
+    public List<Map<String, Object>> func(Map<String, Object> findList) {
+        return ApplicationContextHolder.getBean(DictionaryService.class).findListCache(findList);
+    }
+}
