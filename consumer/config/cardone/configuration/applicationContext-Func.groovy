da51941e0a7061d37c3b8635b1a5cd16e5d05380
addday <addday@gmail.com>
2018-04-08 16:40:17 +0800

    配置优化

---
+++ consumer/config/cardone/configuration/applicationContext-Func.groovy
@@ -0,0 +1,5 @@
+beans {
+    xmlns lang: "http://www.springframework.org/schema/lang"
+
+    lang.'groovy'(id: 'readOneDictionaryByDictionaryTypeCodesFunc', 'script-source': 'file:config/cardone/configuration/groovy/top/cardone/configuration/func/ReadOneDictionaryByDictionaryTypeCodesFunc.groovy')
+}
